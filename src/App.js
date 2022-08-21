d0c2afbb075c8d2e259353108e963811556a07dc
Vadim <hellovados@gmail.com>
2022-08-21 18:24:21 +0300

    WebSocket

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import LongPulling from "./LongPulling";
+import EventSource from "./EventSource";
+import Webs from "./Webs";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Webs />
     </div>
   );
 }
